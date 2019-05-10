650d5cb9090c4ca516694e54d5402b6af4d74364
Byeonggon Lee <gonny952@gmail.com>
2019-05-10 15:01:47 +0900

    Basic collision detection
    Former-commit-id: ba6c34b1e8c6508ef1031dc2564ceccd23a53f2b

--- src/systems.js
+++ src/systems.js
@@ -24,18 +24,51 @@ class MovSystem extends System {
   process() {
     for(let entity of this.state.entities) {
       if(this.filter(entity) == true) {
-        entity.comps['mov'].velX += entity.comps['mov'].accX;
-        entity.comps['mov'].accX = 0;
-        entity.comps['mov'].velY += entity.comps['mov'].accY;
-        entity.comps['mov'].accY = 0;
-        entity.comps['pos'].x += entity.comps['mov'].velX;
-        entity.comps['pos'].y += entity.comps['mov'].velY;
+        const mov = entity.comps['mov'];
+        const pos = entity.comps['pos'];
+        mov.velX += mov.accX;
+        mov.accX = 0;
+        mov.velY += mov.accY;
+        mov.accY = 0;
+        pos.x += mov.velX;
+        pos.y += mov.velY;
       }
     }
   }
 }
 
 class CollSystem extends System {
+  static checkColl(entity1, entity2) {
+    const pos1 = entity1.comps['pos'];
+    const size1 = entity1.comps['size'];
+    const pos2 = entity2.comps['pos'];
+    const size2 = entity2.comps['size'];
+    return (pos1.x + size1.width > pos2.x &&
+            pos2.x + size2.width > pos1.x &&
+            pos1.y + size1.height > pos2.y &&
+            pos2.y + size2.height > pos1.y);
+  }
+  constructor({state, compNames}) {
+    super({state, compNames: ['pos', 'size']});
+  }
+  process() {
+    for(let i = this.state.entities.length - 1; i >= 1; --i) {
+      let entity1 = this.state.entities[i];
+      if(this.filter(entity1)) {
+        for(let j = i - 1; j >= 0; --j) {
+          let entity2 = this.state.entities[j];
+          if(this.filter(entity2)) {
+            if(CollSystem.checkColl(entity1, entity2)) {
+              entity1.comps['size'].width = 100;
+              entity2.comps['size'].width = 100;
+              entity1.comps['size'].height = 100;
+              entity2.comps['size'].height = 100;
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 class CamOutSystem extends System {
@@ -50,7 +83,7 @@ class CamOutSystem extends System {
       let camOut = entity.comps['camOut'];
       let pos = entity.comps['pos'];
       let size = entity.comps['size'];
-      if(this.filter(entity) == true && camOut) {
+      if(this.filter(entity) == true) {
         if(camOut.type == CamOutComp.DESTROY) {
           if((pos.x + size.width - camera.x) * camera.scale < 0 ||
              (pos.x - camera.x) * camera.scale > canvas.width ||
