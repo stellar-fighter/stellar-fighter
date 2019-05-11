4100a34f67e587f2b9da7a49a76382ec2f83485f
Byeonggon Lee <gonny952@gmail.com>
2019-05-11 19:11:09 +0900

    Add Bullet001
    Former-commit-id: 180fa0d278c1e35e200637cea645b155a6962367

--- src/entities.js
+++ src/entities.js
@@ -3,6 +3,13 @@ class Entity {
   constructor({state, comps}) {
     this.state = state;
     this.comps = comps || {};
+    //this.comps = {};
+    for(let comp in comps) {
+      console.log(comps);
+      console.log('------------');
+      console.log(comp.name);
+      this.addComp(comp);
+    }
   }
   addComp(comp) {
     this.comps[comp.name] = comp;
@@ -15,8 +22,6 @@ class Entity {
 class F001 extends Entity {
   constructor({state, comps}) {
     super({state, comps});
-    for(let comp in comps)
-      this.addComp(comp);
     if(this.comps['pos'] === undefined)
       this.addComp(new PosComp({}));
     if(this.comps['mov'] === undefined)
@@ -39,4 +44,26 @@ class F001 extends Entity {
 class A001 extends Entity {
 }
 
-export {Entity, F001, A001};
+class Bullet001 extends Entity {
+  constructor({state, comps}) {
+    super({state, comps});
+    if(this.comps['pos'] === undefined)
+      this.addComp(new PosComp({}));
+    if(this.comps['mov'] === undefined)
+      this.addComp(new MovComp({velY: -30}));
+    if(this.comps['size'] === undefined)
+      this.addComp(new SizeComp({width: 100, height: 500}));
+    if(this.comps['vis'] === undefined)
+      this.addComp(new VisComp({image: this.state.game.assets.fire}));
+    if(this.comps['camOut'] === undefined)
+      this.addComp(new CamOutComp({}));
+    if(this.comps['coll'] === undefined)
+      this.addComp(new CollComp({damage: 1}));
+    if(this.comps['hp'] === undefined)
+      this.addComp(new HpComp({value: 1}));
+    if(this.comps['team'] === undefined)
+      this.addComp(new TeamComp({value: 'PLAYER'}));
+  }
+}
+
+export {Entity, F001, A001, Bullet001};
