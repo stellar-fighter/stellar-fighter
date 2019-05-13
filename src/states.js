4b4af4fe21c550728ce6a64d09d5c3ced214e28b
Byeonggon Lee <gonny952@gmail.com>
2019-05-13 14:37:17 +0900

    Replace array of entries with the entry manager

--- src/states.js
+++ src/states.js
@@ -1,17 +1,23 @@
 import {Camera} from './camera';
-import {Fighter001, Alien001, Bullet001} from './entities';
+import {EntityMan, Fighter001, Alien001, Bullet001} from './entities';
 import {PosComp, SizeComp, MovComp, VisComp, CamOutComp, CollComp, HpComp, TeamComp, ShootingComp} from './comps';
 import {MovSystem, CamOutSystem, CollSystem, HpSystem} from './systems';
 
 class State {
-  constructor({game, running, systems, entities}) {
+  constructor({game, running, systems, entityMan}) {
     this.game = game;
     this.running = running || false;
     this.systems = systems || [];
-    this.entities = entities || [];
+    this.entityMan = entityMan || new EntityMan({});
     this.startTime = null;
     this.curTime = null;
     this.deltaTime = null;
+  }
+  get entities() {
+    return this.entityMan.entities;
+  }
+  set entities(entities) {
+    this.entityMan.entities = entities;
   }
   setTime(timeStamp) {
     if(this.startTime === null)
@@ -26,8 +32,8 @@ class State {
 }
 
 class PlayState extends State {
-  constructor({game, running, systems, entities, canvas, level}) {
-    super({game, running, systems, entities});
+  constructor({game, running, systems, entityMan, canvas, level}) {
+    super({game, running, systems, entityMan});
     this.event = {up: false, down: false, left: false, right: false};
     this.canvas = canvas;
     this.camera = new Camera({canvas, x: 0, y: 0});
@@ -38,17 +44,17 @@ class PlayState extends State {
     this.systems.push(new HpSystem({state: this}));
     this.systems.push(new CamOutSystem({state: this}));
     this.systems.push(new CollSystem({state: this}));
-    this.player = new Fighter001({
-      state: this,
-      comps: {
-        pos: new PosComp({x: 1500, y: 2000}),
-        mov: new MovComp({velY: -15}),
-        camOut: new CamOutComp({type: CamOutComp.BLOCK}),        
-        shooting: new ShootingComp({coolTime:150}),
-      }
-    });
-    this.entities.push(this.player);
-
+    this.playerId = this.entityMan.add(
+      new Fighter001({
+        state: this,
+        comps: {
+          pos: new PosComp({x: 1500, y: 2000}),
+          mov: new MovComp({velY: -15}),
+          camOut: new CamOutComp({type: CamOutComp.BLOCK}),
+          shooting: new ShootingComp({coolTime: 1}),
+        }
+      })
+    );
     const that = this;
     this.handleKeyDown = (event) => {
       console.log(event.code);
@@ -71,10 +77,10 @@ class PlayState extends State {
     while(entityData && this.camera.y <= entityData.y) {
       switch(entityData.type) {
       case 'a-001':
-        //this.entities.push(new Alien001({state: this}));
+        //this.entityMan.add(new Alien001({state: this}));
         break;
       case 's-fighter':
-        this.entities.push(
+        this.entityMan.add(
           new Fighter001({
             state: this,
             comps: {
@@ -88,7 +94,7 @@ class PlayState extends State {
     }
     */
     if(Math.random() > 0.99) {
-      this.entities.push(
+      this.entityMan.add(
         new Fighter001({
           state: this,
           comps: {
@@ -103,26 +109,30 @@ class PlayState extends State {
   update() {
     if(this.running == false)
       return;
-    const pos = this.player.comps['pos'];
-    if(this.event.ArrowUp)
-      pos.y -= 30;
-    if(this.event.ArrowDown)
-      pos.y += 30;
-    if(this.event.ArrowLeft)
-      pos.x -= 30;
-    if(this.event.ArrowRight)
-      pos.x += 30;
-    if(this.event.Space) {
-      if (this.player.comps['shooting'].enabled) {
-        this.entities.push(
-          new Bullet001({
-            state: this,
-            comps: {
-              pos: new PosComp({x: this.player.comps['pos'].x, y: this.player.comps['pos'].y}),
-            }
-          })
-        );
-      }      
+    const player = this.entityMan.get(this.playerId);
+    if(player) {
+      const pos = player.comps['pos'];
+      const shooting = player.comps['shooting'];
+      if(this.event.ArrowUp)
+        pos.y -= 30;
+      if(this.event.ArrowDown)
+        pos.y += 30;
+      if(this.event.ArrowLeft)
+        pos.x -= 30;
+      if(this.event.ArrowRight)
+        pos.x += 30;
+      if(this.event.Space) {
+        if(shooting.enabled) {
+          this.entityMan.add(
+            new Bullet001({
+              state: this,
+              comps: {
+                pos: new PosComp({x: pos.x, y: pos.y}),
+              }
+            })
+          );
+        }
+      }
     }
     this.genEntity();
     for(let system of this.systems) {
@@ -135,57 +145,44 @@ class PlayState extends State {
     const ctx = this.ctx;
     const camera = this.camera;
     const canvas = this.canvas;
-    const entities = this.entities;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.rect(
-      0,
-      0,
-      canvas.width / 2,
-      canvas.height / 2
-    );
-    ctx.stroke();
-
-    if(entities.length > 0) {
-      for(let entity of entities) {
-        const pos = entity.comps['pos'];
-        const size = entity.comps['size'];
-        const vis = entity.comps['vis'];
-        const hp = entity.comps['hp'];
-        ctx.save();
-        ctx.setTransform(
-          camera.scale, 0,
-          0, camera.scale,
-          //size.width / -2 * this.camera.scale, 0 
-          0, 0
-        );
-        ctx.drawImage(
-          vis.image,
-          (pos.x - camera.x),
-          (pos.y - camera.y),
-          size.width,
-          size.height
-        );
-        ctx.beginPath();
-        ctx.rect(
-          (pos.x - camera.x),
-          (pos.y - camera.y),
-          size.width,
-          size.height
-        );
-        ctx.stroke();
-        ctx.closePath();
-        ctx.beginPath();
-        ctx.fillStyle = '#00FF00';
-        ctx.font = '' + (300 * camera.scale) + 'px Arial';
-        ctx.fillText(
-          hp.value,
-          pos.x - camera.x,
-          pos.y - camera.y
-        );
-        ctx.closePath();
-        ctx.restore();
-      }
+    for(let [id, entity] of Object.entries(this.entities)) {
+      const pos = entity.comps['pos'];
+      const size = entity.comps['size'];
+      const vis = entity.comps['vis'];
+      const hp = entity.comps['hp'];
+      ctx.save();
+      ctx.setTransform(
+        camera.scale, 0,
+        0, camera.scale,
+        0, 0
+      );
+      ctx.drawImage(
+        vis.image,
+        (pos.x - camera.x),
+        (pos.y - camera.y),
+        size.width,
+        size.height
+      );
+      ctx.beginPath();
+      ctx.rect(
+        (pos.x - camera.x),
+        (pos.y - camera.y),
+        size.width,
+        size.height
+      );
+      ctx.stroke();
+      ctx.closePath();
+      ctx.beginPath();
+      ctx.fillStyle = '#00FF00';
+      ctx.font = '' + (300 * camera.scale) + 'px Arial';
+      ctx.fillText(
+        hp.value,
+        pos.x - camera.x,
+        pos.y - camera.y
+      );
+      ctx.closePath();
+      ctx.restore();
     }
   }
   destroy() {
