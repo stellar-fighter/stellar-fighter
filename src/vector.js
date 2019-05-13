7a77d0ad8d7a55a24ccc07c0eeffe8c682325407
Byeonggon Lee <gonny952@gmail.com>
2019-05-13 23:56:44 +0900

    Fix errors in Vector, Add more lints
    Former-commit-id: d12423ccf33dc870ae165460aea1d17c5654ee39

--- src/vector.js
+++ src/vector.js
@@ -1,33 +1,44 @@
-class Vector { 
-    // 수정완료
-    //atan2 아크탄젠트 함수는 범위가 -pi ~ pi이라서 모든 점 표현 가능 sin은 -pi/2 ~ pi/2  cos은  0 ~ pi인거 같아서 오류가 있을 수있음 
-  constructor(x, y) { // coordinate x, y 
-    if ( x == undefined || y == undefined) {
-        this.x = 0;
-        this.y = 0;
-    }
-    else {
-        this.x = x;
-        this.y = y;
-    }
-
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    if(this.x === undefined)
+      this.x = 0;
+    if(this.y === undefined)
+      this.y = 0;
   }
-  get angle() { // return radian 
-    return Math.atan2(this.y, this.x)
+  fromAngle(angle, mag) {
+    return new Vector(mag * Math.cos(angle), mag * Math.sin(angle));
   }
-  get mag() {
-    return Math.sqrt(this.x^2 + this.y^2);
-  }
-  fromAngle(angle,mag) {
-    return new Vector(mag * cos(angle), mag * sin(angle));
-  }
-  fromCoords(x, y) {    
+  fromCoords(x, y) {
     return new Vector(x, y);
   }
-  fromVector(vector) { 
+  fromVector(vector) {
     return new Vector(vector.x, vector.y);
   }
-  add(vector) { //getter 이용하여 vector의 x,y값을 얻고  새로운 벡터 생성
+  get angle() {
+    return Math.atan2(this.y, this.x);
+  }
+  get mag() {
+    return Math.sqrt(this.x ^ 2 + this.y ^ 2);
+  }
+  setCoords(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  setVector(vector) {
+    this.x = vector.x;
+    this.y = vector.y;
+  }
+  set angle(angle) {
+    this.x = this.mag * Math.cos(angle);
+    this.y = this.mag * Math.sin(angle);
+  }
+  set mag(mag) {
+    this.x = mag * this.x / this.mag;
+    this.y = mag * this.y / this.mag;
+  }
+  add(vector) {
     this.x + vector.x;
     this.y + vector.y;
   }
@@ -35,29 +46,14 @@ class Vector {
     this.x - vector.x;
     this.y - vector.y;
   }
-  mult(scalar){
-    this.x  =  scalar * this.x;
-    this.y  =  scalar * this.y;
+  mult(scalar) {
+    this.x = scalar * this.x;
+    this.y = scalar * this.y;
   }
-  div(scalar){
+  div(scalar) {
     this.x = this.x / scalar;
     this.y = this.y / scalar;
   }
-  setAngle(angle) { //cos과 sin은 angle이 radian 이여야함
-    this.x = this.mgag * Math.cos(angle);
-    this.y = this.mag * Math.sin(angle);
-  }
-  setCoords(x,y){
-    this.x = x;
-    this.x = y;
-  }
-  setVector(vector){
-    this.x = vector.x;
-    this.y = vector.y;
-  }
-  setMag(mag){ //현재 벡터의 크기로 나누면 크기가 1 다시 스칼라 배를 하면 원하는 크기 가짐
-    this.x  = mag * this.x / this.mag; 
-    this.y = mag * this.y / this.mag;
-  }
 }
+
 export {Vector};
