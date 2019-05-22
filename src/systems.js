74f5499e3ade68a65d52be6dd677dce3ed9dcb4d
Byeonggon Lee <gonny952@gmail.com>
2019-05-22 10:01:56 +0900

    Refactor
    Former-commit-id: 17bf6a83870ad8067214637ede2b26337d6b048b
    Former-commit-id: fa9a88d2148b5a6ecbe91f32920c7f74bd524b92
    Former-commit-id: e065eac2f32691f5c5f2eface231d51539c8a054
    Former-commit-id: 8b5358226793b4c1180d3b2a9663f8b57479e55e
    Former-commit-id: 49cc556ec97d381c98258318b97d1f5f54e3d986
    Former-commit-id: 5181e55d0dde9e1581160e676daa136c34538e4d

--- src/systems.js
+++ src/systems.js
@@ -89,7 +89,7 @@ class CamOutSystem extends System {
       const pos = entity.comps['pos'];
       const size = entity.comps['size'];
       if(this.filter(entity) == true) {
-        if(entity.name == "Boss") {
+        if(entity.name == "alien002") {
           const pivot = 15 * entity.direction;
           const pivot2 = 30 * entity.directionY;
           pos.vec.y += pivot2;
@@ -97,7 +97,7 @@ class CamOutSystem extends System {
           if((pos.vec.y + size.vec.y - camera.pos.y) * camera.scale < 10) {
             entity.directionY = 1;
           }
-          if( (pos.vec.y - camera.pos.y) * camera.scale > canvas.height - 50) {
+          if((pos.vec.y - camera.pos.y) * camera.scale > canvas.height - 50) {
             entity.directionY = -2;
           }
           if((pos.vec.x + size.vec.x - camera.pos.x) * camera.scale < 10) {
@@ -107,7 +107,7 @@ class CamOutSystem extends System {
             entity.direction *= -1;
           }
         }
-        if(entity.name == "Potion") {
+        if(entity.name == "item001") {
           pos.vec.y -= 10 + 10 * Math.sin(Date.now());
           const pivot = 15 * entity.direction;
           pos.vec.x += pivot;
