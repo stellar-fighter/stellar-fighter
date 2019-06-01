c667ca25cd51a72b098488040fa8a616e776492a
Byeonggon Lee <gonny952@gmail.com>
2019-06-02 00:31:24 +0900

    Support testing

---
+++ test/webpack.config-test.js
@@ -0,0 +1,6 @@
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node', // webpack should compile node compatible code
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+};
